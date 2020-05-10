e3b0efe1c8d12cc72071217f9d86ca8597f24555
Michael Kelly <michael@kelly.media>
2020-05-10 00:15:28 -0400

    add prompts and validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,20 @@ create.addEventListener("click", function () {
     pw = generatePassword();
     document.getElementById("password").placeholder = pw;
 });
+
+function generatePassword() {
+    enter = parseInt(prompt("How long should your password be? Enter a number between 8 and 128"));
+    // user validation 
+    if (!enter) {
+        alert("This needs a value");
+    } else if (enter < 8 || enter > 128) {
+
+        enter = parseInt(prompt("You must choose a number between 8 and 128"));
+
+    } else {
+        checkNumber = confirm("Would you like it to include numbers?");
+        checkCharacter = confirm("Would you like it to include special characters?");
+        checkUppercase = confirm("Would you like it to include uppercase letters?");
+        checkLowercase = confirm("Would you like it to include lowercase letters?");
+    };
+}
